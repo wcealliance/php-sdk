3cdb327c54f286a9bca260e88790e11d49421d89
ekhaled <me.khaled@gmail.com>
2013-12-05 15:45:35 +0000

    some inline docs

--- lib/Verified.php
+++ lib/Verified.php
@@ -6,29 +6,48 @@
         private $api_secret;
         private $config = array();
 
+        /**
+         * Constructor
+         * Extends the default config array and sets the values of api_key and api_secret
+         *
+         * @param array $config
+         */
         public function __construct($config = array())
         {
+            //declare default options
             $defaults = array(
                 "envelope"       => true,
                 "suppress_errors" => false,
                 "response_type"   => 'json'
             );
             foreach ($config as $key => $value) {
+                //pluck the api key out of the config array
                 if ($key == "api_key") {
                     $this->api_key = $value;
                     continue;
                 }
+                //pluck the api secret out of the config array
                 if ($key == "api_secret") {
                     $this->api_secret = $value;
                     continue;
                 }
+                //overwrite the default value of config item if it exists
                 if (isset($defaults[$key])) {
                     $defaults[$key] = $value;
                 }
             }
+
+            //store the config back into the class property
             $this->config = $defaults;
         }
 
+        /**
+         * Setter for $api_key
+         * returns the class itself so calls are chainable
+         *
+         * @param string $api_key
+         * @return $this
+         */
         public function setKey($api_key)
         {
             $this->api_key = $api_key;
@@ -36,6 +55,13 @@
             return $this;
         }
 
+        /**
+         * Setter for $api_secret
+         * returns the class itself so calls are chainable
+         *
+         * @param string $api_secret
+         * @return $this
+         */
         public function setSecret($api_secret)
         {
             $this->api_secret = $api_secret;
@@ -43,6 +69,14 @@
             return $this;
         }
 
+        /**
+         * Setter for config items
+         * returns the class itself so calls are chainable
+         *
+         * @param string $key
+         * @param string $val
+         * @return $this
+         */
         public function setConfig($key, $val)
         {
             if (isset($this->config[$key])) {
@@ -52,6 +86,12 @@
             return $this;
         }
 
+        /**
+         * Getter for config items
+         *
+         * @param string $key
+         * @return mixed
+         */
         public function getConfig($key)
         {
             if (isset($this->config[$key])) {
