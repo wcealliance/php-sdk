68a2f018c5f6b0d6423b7be1b2d1220b7d918728
ekhaled <me.khaled@gmail.com>
2014-04-02 17:50:51 +0100

    unit tests for method call parser

--- tests/unit/methodTest.php
+++ tests/unit/methodTest.php
@@ -52,6 +52,90 @@ class methodTest extends baseTest
         
     }
     
+    public function testMethodCallParser()
+    {
+        $e = $this->V->getConfig('api_endpoint');
+        $v = 'v' . $this->V->getConfig('api_version');
+        $data = array(
+            'getMe' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/'
+            ),
+            'getMeSomething' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/',
+                'sub_resource' => 'something'
+            ),
+            'getMeSomethingElse' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/',
+                'sub_resource' => 'somethingElse'
+            ),
+            'editMe' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/'
+            ),
+            'editMeSomewhere' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/',
+                'sub_resource' => 'somewhere'
+            ),
+            'editMeSomewhereElse' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/',
+                'sub_resource' => 'somewhereElse'
+            ),
+            'deleteMe' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/'
+            ),
+            'deleteMeHere' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/',
+                'sub_resource' => 'here'
+            ),
+            'deleteMeWhereEver' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/',
+                'sub_resource' => 'whereEver'
+            ),
+            'addMe' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/'
+            ),
+            'addMeThing' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/',
+                'sub_resource' => 'thing'
+            ),
+            'addMeAnotherThing' => array(
+                'endpoint' => implode("/", array($e . $v, 'me')) . '/',
+                'sub_resource' => 'anotherThing'
+            ),
+            'getReportThing' => array(
+                'endpoint' => implode("/", array($e . $v, 'report')) . '/' . 'thing' . '/',
+            ),
+            'getReportAnotherThing' => array(
+                'endpoint' => implode("/", array($e . $v, 'report')) . '/' . 'anotherThing' . '/',
+            ),
+            'editReportThing' => array(
+                'endpoint' => implode("/", array($e . $v, 'report')) . '/' . 'thing' . '/',
+            ),
+            'editReportAnotherThing' => array(
+                'endpoint' => implode("/", array($e . $v, 'report')) . '/' . 'anotherThing' . '/',
+            ),
+            'deleteReportThing' => array(
+                'endpoint' => implode("/", array($e . $v, 'report')) . '/' . 'thing' . '/',
+            ),
+            'deleteReportAnotherThing' => array(
+                'endpoint' => implode("/", array($e . $v, 'report')) . '/' . 'anotherThing' . '/',
+            ),
+            'addReportThing' => array(
+                'endpoint' => implode("/", array($e . $v, 'report')) . '/' . 'thing' . '/',
+            ),
+            'addReportAnotherThing' => array(
+                'endpoint' => implode("/", array($e . $v, 'report')) . '/' . 'anotherThing' . '/',
+            )
+        );
+        
+        foreach ($data as $k => $v) {
+            $returned = $this->call('parseMethodCall', $k);
+            $this->assertSame($v['endpoint'], $returned['endpoint']);
+            if (isset($v['sub_resource'])) {
+                $this->assertSame($v['sub_resource'], $returned['sub_resource']);
+            }
+        }
+    }
+    
     
     private function getSignature($request_time, $method, $endpoint, $secret)
     {
